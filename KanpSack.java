d5c99439f2d50f820c1eeac360bb004ec3013712
Virender Singh <virendersinghdev1@gmail.com>
2024-04-09 11:50:39 +0530

    Update KanpSack.java

--- KanpSack.java
+++ KanpSack.java
@@ -33,25 +33,67 @@ public class Solution {
     }
 
     public static int solveTab(int[] weight, int[] value, int n, int maxWeight) {
-        int[][] dp = new int[n+1][maxWeight + 1];
-        
-        
 
-        for (int index = n-1; index >= 0; index--) {
+        int[][] dp = new int[n + 1][maxWeight + 1];
+
+        for (int index = n - 1; index >= 0; index--) {
             for (int w = 0; w <= maxWeight; w++) {
                 int incl = Integer.MIN_VALUE;
-                if (weight[index] <= w )
-                    incl = value[index] + dp[index+1][w - weight[index]];
-                    
+                if (weight[index] <= w)
+                    incl = value[index] + dp[index + 1][w - weight[index]];
+
                 int excl = dp[index + 1][w];
 
                 dp[index][w] = Math.max(incl, excl);
             }
         }
         return dp[0][maxWeight];
+
+    }
+
+    public static int solveSO1(int[] weight, int[] value, int n, int maxWeight) {
+
+        int[] curr = new int[maxWeight + 1];
+        int[] next = new int[maxWeight + 1];
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int w = 0; w <= maxWeight; w++) {
+                int incl = 0;
+                if (weight[index] <= w)
+                    incl = value[index] + next[w - weight[index]];
+
+                int excl = next[w];
+
+                curr[w] = Math.max(incl, excl);
+            }
+            // Copy values from next to curr
+            System.arraycopy(curr, 0, next, 0, maxWeight + 1);
+        }
+        return next[maxWeight];
+
+    }
+
+    public static int solveSO2(int[] weight, int[] value, int n, int maxWeight) {
+
+        int[] curr = new int[maxWeight + 1];
+        
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int w = maxWeight; w >= 0; w--) {
+                int incl = 0;
+                if (weight[index] <= w)
+                    incl = value[index] + curr[w - weight[index]];
+
+                int excl = curr[w];
+
+                curr[w] = Math.max(incl, excl);
+            }
+        }
+        return curr[maxWeight];
+
     }
 
     static int knapsack(int[] weight, int[] value, int n, int maxWeight) {
-        return solveTab(weight, value, n, maxWeight);
+        return solveSO2(weight, value, n, maxWeight);
     }
 }
